e250eb5e9f5cbd0c3fa11f56a48b88b3028c93f6
Marwwin <markus.kankkonen@protonmail.com>
2023-10-08 22:32:55 +0300

    created AOC cli finished day2

--- aoc_2018/AOC.lua
+++ aoc_2018/AOC.lua
@@ -1,6 +1,13 @@
 local AOC = {}
-function AOC.GetInput()
-	local file = io.open("input", "r")
+
+function AOC.get_input(fileName, use_test_data)
+	local file
+	if use_test_data then
+		file = io.open(fileName .. "/test", "r")
+	else
+		file = io.open(fileName .. "/input", "r")
+	end
+
 	if not file then
 		print("File Open error")
 	else
@@ -15,12 +22,110 @@ function AOC.GetInput()
 	end
 end
 
-function AOC.PrintInput(input, config)
+function AOC.print_input(input, config)
 	for _, v in pairs(input) do
 		io.write(v, " ")
-		if config.newLines then io.write("\n") end
+		if config then
+			if config.newLines then io.write("\n") end
+		end
 	end
 	io.write("\n")
 end
 
+-- UTIL Functions
+
+function AOC.count_table_keys(t)
+	local count = 0
+	for _, _ in pairs(t) do
+		count = count + 1
+	end
+	return count
+end
+
+function AOC.string_intersection(str_a, str_b)
+	local result = ""
+	for i = 1, #str_a do
+		if str_a:sub(i, i) == str_b:sub(i, i) then
+			result = result .. str_a:sub(i, i)
+		end
+	end
+	return result
+end
+
+function AOC.map(t, fn)
+	local result = {}
+	for _, v in pairs(t) do
+		table.insert(result, fn(v))
+	end
+	return result
+end
+
+function AOC.filter(t, fn)
+	local result = {}
+	for _, v in pairs(t) do
+		if fn(v) then table.insert(result, v) end
+	end
+end
+
+function AOC.at(str, i)
+	return str:sub(i, i)
+end
+
+function AOC.reduce(t, fn, initalValue)
+	for _, v in pairs(t) do
+		initalValue = fn(initalValue, v)
+	end
+	return initalValue
+end
+
+-- CLI Commands
+
+local args = { ... }
+
+
+
+function AOC.parse_arg()
+	local config = {}
+	for _, arg in ipairs(args) do
+		if arg == "--input" or arg == "-i" then config["show_input_data"] = true end
+		if arg == "--test" or arg == "-t" then config["test_data"] = true end
+	end
+
+	for i, arg in ipairs(args) do
+		if arg == "run" then AOC.run_day(args[i + 1], config) end
+		if arg == "create" then AOC.create(args[i + 1], config) end
+	end
+end
+
+function AOC.create(day_number, config)
+	os.execute("mkdir " .. day_number)
+	os.execute("touch " .. day_number .. "/input")
+	os.execute("touch " .. day_number .. "/test")
+	local init_solution = [[
+local AOC = require("AOC")
+local day = {}
+
+function day:part1(input_data)
+end
+
+function day:part2(input_data)
+end
+
+return day
+	]]
+	os.execute("echo '" .. init_solution .. "' >> " .. day_number .. "/solution.lua")
+end
+
+function AOC.run_day(day_number, config)
+	local data = AOC.get_input(day_number, config.test_data)
+	if config.show_input_data then
+		AOC.print_input(data)
+	end
+	local day = require(day_number .. "/solution")
+	print("part1: ", day:part1(data))
+	print("part2: ", day:part2(data))
+end
+
+
+AOC.parse_arg()
 return AOC
--- aoc_2018/day2/solution.lua
+++ aoc_2018/day2/solution.lua
@@ -1,3 +1,76 @@
-local AOC = require "AOC"
+local AOC = require("AOC")
+local day = {}
 
-AOC:PrintInput(AOC:GetInput())
+local function get_letters(word)
+	local letters = {}
+	for j = 1, #word do
+		local char = word:sub(j, j)
+		local l = letters[char]
+		if l then
+			letters[char] = l + 1
+		else
+			letters[char] = 1
+		end
+	end
+	return letters
+end
+
+function day:part1(input_data)
+	local threes = 0
+	local twos = 0
+	for i, str in ipairs(input_data) do
+		local letters = get_letters(str)
+		local two_found = false
+		local three_found = false
+		for j, v in pairs(letters) do
+			if two_found and three_found then break end
+			if v == 2 and not two_found then
+				twos = twos + 1
+				two_found = true
+			end
+			if v == 3 and not three_found then
+				threes = threes + 1
+				three_found = true
+			end
+		end
+	end
+	return threes * twos
+end
+
+local Word = {}
+
+function Word.create(word)
+	local mt = {
+		__sub = function(a, b)
+			local dif = {}
+			for i = 1, #a.word do
+				local a_char, b_char = a.word:sub(i, i), b.word:sub(i, i)
+				if not (a_char == b_char) then
+					table.insert(dif, a_char)
+					table.insert(dif, b_char)
+				end
+			end
+			if AOC.count_table_keys(dif) == 2 then
+				return dif[1]:sub(1, 1):byte() - dif[2]:sub(1, 1):byte()
+			end
+			return 0
+		end
+	}
+	local o = {}
+	o.word = word
+	setmetatable(o, mt)
+	return o
+end
+
+function day:part2(input_data)
+	local words = AOC.map(input_data, Word.create)
+	for i, word in ipairs(words) do
+		for j = i + 1, AOC.count_table_keys(words) do
+			if math.abs(word - words[j]) == 1 then
+				return AOC.string_intersection(word.word, words[j].word)
+			end
+		end
+	end
+end
+
+return day
