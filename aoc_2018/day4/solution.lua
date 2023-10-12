b7e9266c4a81aa06d81032d901eeb6d1893b031f
Marwwin <markus.kankkonen@protonmail.com>
2023-10-12 22:05:32 +0300

    small work on day4

--- aoc_2018/day4/solution.lua
+++ aoc_2018/day4/solution.lua
@@ -10,51 +10,82 @@ local Action = {
 day.Action = Action
 
 function day:part1(input_data)
+  local events = AOC.map(input_data, day.parse_input)
+  events = day.sort_events(events)
+  events = day.count_sleep(events)
 end
 
 function day:part2(input_data)
 end
 
-function day.sort_events(events)
-  events = AOC.map(events,
-    function(t)
-      t.time = os.time({
-        year = t.year,
-        month = t.month,
-        day = t.day,
-        hour = t.hour,
-        min = t.minute
-      })
-      return t
-    end)
-  table.sort(events, day.compare_by_time)
-  return events
+function day.count_sleep(events)
+  local result = {}
+  local id = 0
+  for _, event in ipairs(events) do
+    print(event.id, event.day, event.action)
+    if event.action == Action.START_WORK then
+      id = event.id
+      if not result[id] then
+        result[id] = {}
+        result[id][event.day] = {}
+      end
+    end
+    if event.action == Action.FALL_ASLEEP then
+      result[id][event.day].fall_asleep = event.minute
+    end
+    if event.action == Action.WAKE_UP then
+      result[id][event.day].wakeup = event.minute
+      result[id][event.day].slept = event.minute - result[id][event.day].fall_asleep
+    end
+  end
+  return result
 end
 
-function day.compare_by_time(ev1, ev2)
-  return ev1.time < ev2.time
+function day.most_sleep(events)
+  local slept = AOC.map(events, function(guard)
+    AOC.reduce(guard, function(acc, cur)
+      return acc + cur.slept
+    end, 0)
+  end)
+  table.sort(slept, function(g1, g2) return g1 < g2 end)
+  AOC.print_t(slept)
+end
+
+function day.sort_events(events)
+  table.sort(events, function(ev1, ev2)
+    return ev1.time < ev2.time
+  end
+  )
+  return events
 end
 
 function day.parse_input(str)
   str = str:gsub("[-:%[%]#]", " ")
-  local result = {}
+  local event = {}
   local split = AOC.split(str, " ")
   for i, v in ipairs(split) do
-    if i == 1 then result.year = tonumber(v) end
-    if i == 2 then result.month = tonumber(v) end
-    if i == 3 then result.day = tonumber(v) end
-    if i == 4 then result.hour = tonumber(v) end
-    if i == 5 then result.minute = tonumber(v) end
+    if i == 1 then event.year = tonumber(v) end
+    if i == 2 then event.month = tonumber(v) end
+    if i == 3 then event.day = tonumber(v) end
+    if i == 4 then event.hour = tonumber(v) end
+    if i == 5 then event.minute = tonumber(v) end
     if i == 6 then
       if v == "Guard" then
-        result.id = tonumber(split[i + 1])
-        result.action = Action.START_WORK
+        event.id = tonumber(split[i + 1])
+        event.action = Action.START_WORK
       end
-      if v == "falls" then result.action = Action.FALL_ASLEEP end
-      if v == "wakes" then result.action = Action.WAKE_UP end
+      if v == "falls" then event.action = Action.FALL_ASLEEP end
+      if v == "wakes" then event.action = Action.WAKE_UP end
     end
   end
-  return result
+  event.time = os.time({
+    year = event.year,
+    month = event.month,
+    day = event.day,
+    hour = event.hour,
+    min = event.minute
+  })
+  return event
 end
 
 return day
--- aoc_2018/day4/test.lua
+++ aoc_2018/day4/test.lua
@@ -11,6 +11,7 @@ describe("parse input", function()
       day = 1,
       hour = 00,
       minute = 00,
+      time = -14237516389,
       action = day4.Action.START_WORK
     }, result)
   end)
@@ -23,6 +24,7 @@ describe("parse input", function()
       day = 1,
       hour = 00,
       minute = 05,
+      time = -14237516089,
       action = day4.Action.FALL_ASLEEP
     }, result)
   end)
@@ -34,6 +36,7 @@ describe("parse input", function()
       day = 1,
       hour = 00,
       minute = 25,
+      time = -14237514889,
       action = day4.Action.WAKE_UP
     }, result)
   end)
@@ -48,5 +51,38 @@ describe("events", function()
     local sorted_events = day4.sort_events(events)
     assert.is_true(sorted_events[1].time < sorted_events[2].time)
     assert.is_true(sorted_events[2].time < sorted_events[3].time)
+  end)
+  it("should count sleeping time for one event", function()
+    local string_events = { "[1518-11-01 00:25] wakes up",
+      "[1518-11-01 00:05] falls asleep",
+      "[1518-11-01 00:00] Guard #10 begins shift",
+      "[1518-11-01 00:30] Guard #11 begins shift" }
+    local events = AOC.map(string_events, day4.parse_input)
+    local sorted_events = day4.sort_events(events)
+    assert.are.same({ [10] = { [1] = { fall_asleep = 5, wakeup = 25, slept = 20 } }, [11] = { [1] = {} } },
+      day4.count_sleep(sorted_events))
+  end)
+  it("should find who slept most", function()
+    local events = { "[1518-11-01 00:05] falls asleep",
+      "[1518-11-01 00:00] Guard #10 begins shift",
+      "[1518-11-01 00:25] wakes up",
+      "[1518-11-01 00:30] falls asleep",
+      "[1518-11-01 00:55] wakes up",
+      "[1518-11-03 00:24] falls asleep",
+      "[1518-11-03 00:05] Guard #10 begins shift",
+      "[1518-11-02 00:40] falls asleep",
+      "[1518-11-02 00:50] wakes up",
+      "[1518-11-04 00:02] Guard #99 begins shift",
+      "[1518-11-05 00:45] falls asleep",
+      "[1518-11-03 00:29] wakes up",
+      "[1518-11-04 00:36] falls asleep",
+      "[1518-11-01 23:58] Guard #99 begins shift",
+      "[1518-11-05 00:55] wakes up",
+      "[1518-11-04 00:46] wakes up",
+      "[1518-11-05 00:03] Guard #99 begins shift", }
+    events = AOC.map(events, day4.parse_input)
+    events = day4.sort_events(events)
+    events = day4.count_sleep(events)
+    assert.is_true(10, day4.most_sleep(events))
   end)
 end)
