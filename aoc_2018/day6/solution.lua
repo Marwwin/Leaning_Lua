61252cd304e35364354d16b099e51efa6cff1fcf
Marwwin <markus.kankkonen@protonmail.com>
2023-10-18 22:23:16 +0300

    finished day 6 part1

--- aoc_2018/day6/solution.lua
+++ aoc_2018/day6/solution.lua
@@ -8,9 +8,19 @@ function day:part1(input_data)
     table.insert(acc, day.parse_input(cur))
     return acc
   end, {}))
+  local infinites = day.find_coordinates_that_go_infinite(coordinates)
   local result = day.generate_closest(coordinates)
-  -- AOC.print_t(result)
-  return #coordinates
+  result = result
+      :filter(function(id) return id ~= -1 and not infinites:contains(id) end)
+      :reduce(function(acc, cur)
+        if not acc[cur] then acc[cur] = 1 else acc[cur] = acc[cur] + 1 end
+        return acc
+      end, FList({}))
+      :reduce(function(acc, cur)
+        if cur > acc then return cur end
+        return acc
+      end, 0, true)
+  return result
 end
 
 function day:part2(input_data)
@@ -19,30 +29,45 @@ end
 function day.generate_closest(list)
   local max_x = math.max(table.unpack(list:map(function(e) return e.x end)))
   local max_y = math.max(table.unpack(list:map(function(e) return e.y end)))
-  local edge = Vec2D(max_x, max_y)
-  local result = {}
-  for y = 0, edge.y, 1 do
-    for x = 0, edge.x, 1 do
-      local closest = math.maxinteger
-      local same = false
-      local id = 0
-      for index, value in ipairs(list) do
-        local distance = value:manhattan(x, y)
-        if distance == closest then same = true end
-        if distance < closest then
-          closest = distance
-          id = index
-          same = false
-        end
-      end
-      if same then
-      else
-        table.insert(result, id)
-      end
+  local result = FList({})
+  for y = 0, max_y, 1 do
+    for x = 0, max_x, 1 do
+      table.insert(result,day.find_closest(list,x,y))
     end
   end
-  AOC.print_t(result)
   return result
+end
+
+function day.find_coordinates_that_go_infinite(list)
+  local max_x = math.max(table.unpack(list:map(function(e) return e.x end)))
+  local max_y = math.max(table.unpack(list:map(function(e) return e.y end)))
+  local result = FList({})
+  for x = 0, max_x, 1 do
+    table.insert(result, day.find_closest(list, x, 0))
+    table.insert(result, day.find_closest(list, x, max_y))
+  end
+  for y = 0, max_y, 1 do
+    table.insert(result, day.find_closest(list, 0, y))
+    table.insert(result, day.find_closest(list, max_x, y))
+  end
+  result = result:unique():filter(function(e) return e > 0 end)
+  return result
+end
+
+function day.find_closest(list, x, y)
+  local closest = math.maxinteger
+  local id = 0
+  local same = false
+  for index, value in ipairs(list) do
+    local distance = value:manhattan(x, y)
+    if distance == closest then same = true end
+    if distance < closest then
+      closest = distance
+      id = index
+      same = false
+    end
+  end
+  if same then return -1 else return id end
 end
 
 function day.parse_input(str)
--- aoc_2018/utils/FList.lua
+++ aoc_2018/utils/FList.lua
@@ -11,8 +11,8 @@ local metatable = {
 
 function F:map(func)
   local result = {}
-  for i, v in ipairs(self) do
-    result[i] = func(v)
+  for i, value in ipairs(self) do
+    result[i] = func(value)
   end
   return F(result)
 end
@@ -27,20 +27,52 @@ function F:filter(func)
   return F(result)
 end
 
-function F:reduce(func, init)
+function F:reduce(func, init, not_indexed)
+  not_indexed = not_indexed or false
   local result = init
-  for _, value in ipairs(self) do
-    result = func(result, value)
+  if not_indexed then
+    for _, value in pairs(self) do
+      result = func(result, value)
+    end
+  else
+    for _, value in ipairs(self) do
+      result = func(result, value)
+    end
   end
   return result
 end
 
 function F:size()
   local result = 0
-  for key, value in ipairs(self) do
+  for _, _ in ipairs(self) do
     result = result + 1
   end
   return result
+end
+
+function F:unique()
+  local result = {}
+  local seen = {}
+  for _, value in ipairs(self) do
+    if not seen[value] then
+      table.insert(result, value)
+      seen[value] = true
+    end
+  end
+  return F(result)
+end
+
+function F:contains(val)
+  for _, value in ipairs(self) do
+    if value == val then return true end
+  end
+  return false
+end
+
+function F:print()
+  for key, value in pairs(self) do
+   if type(value) == "table" then F(value):print() else print(value) end 
+  end
 end
 
 setmetatable(F, metatable)
