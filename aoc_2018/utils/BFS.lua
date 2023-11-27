b873747277f03089819a150cf525eecca32297c7
Marwwin <markus.kankkonen@protonmail.com>
2023-11-27 21:47:04 +0200

    Added depth first search

--- aoc_2018/utils/BFS.lua
+++ aoc_2018/utils/BFS.lua
@@ -2,8 +2,10 @@ local Vec2D = require("utils.Vec2D")
 local Stack = require("utils.Stack")
 local Queue = require("utils.Queue")
 
-local SIZE = 38
+local SIZE = 20
 local M = {}
+
+-- Breadth First Search
 
 M.BFS = function(start, goal, is_walkable, map)
   local q = Queue()
@@ -12,13 +14,12 @@ M.BFS = function(start, goal, is_walkable, map)
 end
 
 M.BFS_ = function(goal, is_walkable, mapp, q, path, seen)
-  local seen = seen or {}
   local next_node = q:pop()
   path[next_node:to_string()] = true
   seen[next_node:to_string()] = true
   for i, neighbour in ipairs(next_node:neighbours(true)) do
     if seen[neighbour:to_string()] == nil and is_walkable(neighbour, mapp) then
-      seen[neighbour:to_string()]= true
+      seen[neighbour:to_string()] = true
       if neighbour.x >= 0 and neighbour.x <= SIZE and neighbour.y >= 0 and neighbour.y <= SIZE then
         q:push(neighbour)
       end
@@ -27,29 +28,60 @@ M.BFS_ = function(goal, is_walkable, mapp, q, path, seen)
   if next_node:equals(goal) then
     M.backtrack(path)
   else
-    M.print_map(path, mapp)
+    M.print_map(path, mapp, goal)
     return M.BFS_(goal, is_walkable, mapp, q, path, seen)
   end
 end
 
-M.backtrack = function(path)
-  for key, value in pairs(path) do
-    print(key, " ", value)
+-- Depth First Search
+
+M.DFS = function(start, goal, is_walkable, map)
+  local q = Stack()
+  q:push(start)
+  return M.BFS_(goal, is_walkable, map, q, {}, {})
+end
+
+M.DFS_ = function(goal, is_walkable, mapp, q, path, seen)
+  local next_node = q:pop()
+  path[next_node:to_string()] = true
+  seen[next_node:to_string()] = true
+  for i, neighbour in ipairs(next_node:neighbours(true)) do
+    if seen[neighbour:to_string()] == nil and is_walkable(neighbour, mapp) then
+      seen[neighbour:to_string()] = true
+      if neighbour.x >= 0 and neighbour.x <= SIZE and neighbour.y >= 0 and neighbour.y <= SIZE then
+        q:push(neighbour)
+      end
+    end
+  end
+  if next_node:equals(goal) then
+   M.backtrack(path)
+  else
+    M.print_map(path, mapp, goal)
+    return M.DFS_(goal, is_walkable, mapp, q, path, seen)
   end
 end
 
-M.print_map = function(m,terrain)
+M.backtrack = function(path)
+  print("GG")
+--   for key, value in pairs(path) do
+--     print(key, " ", value)
+--   end
+end
+
+M.print_map = function(m, terrain, goal)
   os.execute("sleep 0.05")
   os.execute("clear")
   for y = 0, SIZE, 1 do
     for x = 0, SIZE, 1 do
-      local v = x .. " " ..y
-      if m[v] ~= nil then
+      local v = x .. " " .. y
+      if x == goal.x and y == goal.y then
+        io.write(" G ")
+      elseif m[v] ~= nil then
         io.write(" * ")
       elseif terrain[v] then
         io.write(" # ")
       else
-        io.write(" _ ")
+        io.write(" . ")
       end
     end
     io.write("\n")
@@ -57,12 +89,15 @@ M.print_map = function(m,terrain)
 end
 
 
+--
+--TEST DATA
+--
+
 local function walkable(vec, map)
-  if map[vec:to_string()] ~= nil then
-    print("isWalkable")
-    return true
+  if map[vec:to_string()] == true then
+    return false
   end
-  return false
+  return true
 end
 
 local map = {}
@@ -76,10 +111,29 @@ map["8 5"] = true
 map["8 6"] = true
 map["8 7"] = true
 
+map["8 8"] = true
+map["7 8"] = true
+map["6 7"] = true
+map["3 10"] = true
+map["4 10"] = true
+map["5 10"] = true
+map["6 10"] = true
+map["7 10"] = true
+map["8 10"] = true
 
 
+map["10 7"] = true
+map["10 8"] = true
+map["10 9"] = true
+map["10 10"] = true
+map["10 11"] = true
+map["11 11"] = true
+map["12 11"] = true
+map["13 11"] = true
 
-M.BFS(Vec2D(3, 3), Vec2D(10, 10), walkable, map)
+
+-- M.BFS(Vec2D(3, 3), Vec2D(12, 10), walkable, map)
+M.DFS(Vec2D(3, 3), Vec2D(12, 10), walkable, map)
 
 
 
